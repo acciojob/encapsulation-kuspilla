30e904b03fd46fe43bdbf95f04908a4e0c67ef47
kuspilla <118829965+kuspilla@users.noreply.github.com>
2023-07-11 19:33:40 +0530

    job is done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+
+        RWOnly person = new RWOnly();
+
+        // String name = person.member;
+        // you con not directly access private variable using setters and getters function
+
+        person.Setvariable("dasthu");
+        System.out.println(person.Getvariable());
+
+
+          }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private  String member = "name";
+
+    // once declare private not directly use another package only same class use directly
+   public void Setvariable( String member){
+       // modify private variable suing Set variable function
+       this.member = member;
+   }
+   public String Getvariable(){
+       // access another package private variable value
+       return this.member;
+   }
+
 }
